022806482b29123caf1be7ca869d9770c71eb3d9
Ryan Golhar <ryan.golhar@bms.com>
2026-02-19 13:11:42 -0500

    Fix api endpoint

--- batch_event_handler.py
+++ batch_event_handler.py
@@ -15,7 +15,7 @@ def post_job(job_id, job_status, log_stream_name):
     }
 
     headers = {'Content-Type': 'application/json'}
-    url = "%s/api/v0/jobs/%s" % (os.environ['NGS360_API_SERVER'], job_id)
+    url = "%s/api/v1/jobs/%s" % (os.environ['NGS360_API_SERVER'], job_id)
 
     try:
         logger.info("Sending %s to %s", message_body, url)
